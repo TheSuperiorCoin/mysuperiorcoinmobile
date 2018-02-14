870d865d1148e6b649958b18ed7f420368954b7a
Romain Bergery <contact@evaveo.com>
2018-02-14 14:32:06 +0100

    update

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ApplicationProvider } from '../../providers/application/application';
 
-/**
- * Generated class for the AccountPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-account',
   templateUrl: 'account.html',
--- src/pages/send/send.ts
+++ src/pages/send/send.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SendPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-send',
   templateUrl: 'send.html',
