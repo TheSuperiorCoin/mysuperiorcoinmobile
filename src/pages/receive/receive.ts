5090fd0b3cd0973d822b71835f3b878452bcd1ee
Romain Bergery <contact@evaveo.com>
2018-02-14 14:34:14 +0100

    update

--- src/pages/receive/receive.ts
+++ src/pages/receive/receive.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ReceivePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-receive',
   templateUrl: 'receive.html',
